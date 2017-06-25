5dcb5610a53ec5c7438f063c62a6899cd4c34c7b
Young Park <young_park@brown.edu>
2017-06-25 22:31:12 +0900

    Simple recursion to simplify fractions implemented. Will implement I/O to try challenge input.

--- src/SimplifyFractions.java
+++ src/SimplifyFractions.java
@@ -9,20 +9,29 @@ public class SimplifyFractions {
     public SimplifyFractions(int a, int b) {
         this.top = a;
         this.bottom = b;
+        simplify();
     }
 
     private int simplify() {
-        if (this.top % 2 == 0 && this.bottom % 2 == 0) {
+        boolean two = this.top % 2 == 0 && this.bottom % 2 == 0;
+        boolean three = this.top % 3 == 0 && this.bottom % 3 == 0;
+        if (two) {
             this.top /= 2;
             this.bottom /= 2;
-        } else if (this.top % 3 == 0 && this.bottom % 3 == 0) {
+        } else if (three) {
             this.top /= 3;
             this.bottom /= 3;
-        } else {
-            return 0;
         }
-        simplify();
-        return 0; // just to compile
+        // recursive
+        if (two || three) {
+            simplify();
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        SimplifyFractions a = new SimplifyFractions(1536, 78360);
+        System.out.println(a.top + " " + a.bottom);
     }
 
 }
